a5a12e64a5595e6f470bcb7e3210d6706809eaee
daye200 <daye200@naver.com>
2024-02-06 16:40:17 +0900

    dialog테두리 문제 수정

--- app/src/main/java/com/example/alomtest/food/mainpage/Food_dialog.kt
+++ app/src/main/java/com/example/alomtest/food/mainpage/Food_dialog.kt
@@ -2,8 +2,11 @@ package com.example.alomtest.food.mainpage
 
 import android.app.Dialog
 import android.content.Context
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
 import android.widget.TextView
 import com.example.alomtest.R
 
@@ -11,6 +14,7 @@ class Food_dialog(context: Context, private val dataTitle: String) : Dialog(cont
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_food_dialog)
+        window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
         val dialogTitle = findViewById<TextView>(R.id.dialogTitle)
         dialogTitle.text = dataTitle
     }
