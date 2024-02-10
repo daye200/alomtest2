a8df4bd4857813b12415b6f9a216674d11b95727
daye200 <daye200@naver.com>
2024-02-11 01:34:30 +0900

    간단한 디자인 수정

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
     testImplementation ("junit:junit:4.+")
     implementation("it.xabaras.android:recyclerview-swipedecorator:1.4")
 
+
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
